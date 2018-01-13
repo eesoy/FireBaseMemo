ca6a9448869a0ace149e45986e801bdd92c07bd8
SO YOUNG <LEESOYOUNG@itui-macbook-pro.local>
2018-01-13 14:35:42 +0900

    pod, firebase 연동

--- FireBaseMemo/ViewController.swift
+++ FireBaseMemo/ViewController.swift
@@ -7,18 +7,81 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var memoTableView: UITableView!
+    
+    var ref: DatabaseReference!
+    
+    var memoArray = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        memoTableView.dataSource = self
+        ref = Database.database().reference()
+        
+        //저장
+//        ref.child("soyoung").setValue(["라멘", "해장국", "햄버거"])
+        
+//        ref.child("test").setValue([1, 2, 3])
+        
+        //서버에 있는 메모 로드
+        loadMemo()
+        
+        //가져오기
+        ref.child("test").observe(.value) { (snapshot) in
+            for item in snapshot.children {
+                let snapshotItem = item as! DataSnapshot
+                print(snapshotItem.value)
+            }
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func loadMemo() {
+        //가져오기
+        ref.child("soyoung").observe(.value) { (snapshot) in
+            //            for item in snapshot.children {
+            //                let snapshotItem = item as! DataSnapshot
+            //                print(snapshotItem.value)
+            //            }
+            self.memoArray = snapshot.value as! [String]
+            self.memoTableView.reloadData()
+        }
     }
+    
+    func addMemo(memoString: String){
+        memoArray.append(memoString)
+        ref.child("soyoung").setValue(memoArray)
+    }
+    @IBAction func addClicked(_ sender: UIButton) {
+        let alert = UIAlertController(title: "추가", message: "정보 추가", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in
+            self.addMemo(memoString: alert.textFields![0].text!)
+        }))
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "메모를 입력하시오"
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    //MARK: -UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
+        cell.textLabel?.text = memoArray[indexPath
+        .row]
+        return cell
+    }
+    
 
 
 }
